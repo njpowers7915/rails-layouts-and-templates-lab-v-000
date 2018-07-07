21951fff3f8234dc33b32858f50279b5e4eab831
Nicholas Powers <njpowers7915@gmail.com>
2018-07-07 20:02:33 +0000

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -2,7 +2,14 @@ class StoreAdminController < ApplicationController
     layout "admin"
 
     def home
-        render template: 'home'
+    end
+
+    def orders
+        render :layout => "order_administration"
+    end
+
+    def invoice
+        render :layout => false
     end
 
 end
